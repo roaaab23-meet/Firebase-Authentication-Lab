ae936f53bbd28e6ac2b7232d2b49ca90c36493ca
roaaab23-meet <roaa.ab86@gmail.com>
2022-08-01 14:24:13 +0300

    lab done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -15,10 +15,13 @@ firebaseConfig = {
   "messagingSenderId": "552764983814",
   "appId": "1:552764983814:web:17d439f2d039b977d11187",
   "measurementId": "G-GEHY5GKK1L" ,
-  "databaseURL" : ""
+  "databaseURL" : "https://authentication-lab-90b38-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 firebase=pyrebase.initialize_app(firebaseConfig)
 auth=firebase.auth()
+db= firebase.database()
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -44,6 +47,8 @@ def signup():
         password=request.form['password']
         try:
             login_session['user']=auth.create_user_with_email_and_password(email,password)
+            user={"Email":request.form['Email'], "password":request.form['[password'], "full name":erquest.form['full_name'],"username":request.form['username'], "bio":request.form['bio']}
+            db.child("user").child(login_session['user']['localId']).set(user) 
             return redirect(url_for('home'))
 
         except:
@@ -56,9 +61,21 @@ def signout():
     login_session['user'] = None
     auth.current_user = None
     return redirect(url_for('signin'))
+
+
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method=='POST':
+        tweet={"title":request.form['title'],"text":request.form['text'], "uid":login_session['user']['localId']}
+        db.child("Tweets").push(tweet)
     return render_template("add_tweet.html")
+
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def tweets():
+    if request.method=='GET':
+        tweet_dict = db.child("Tweets").get().val()
+    return render_template("tweets.html", tweet_dict=tweet_dict)
 
 
 if __name__ == '__main__':
